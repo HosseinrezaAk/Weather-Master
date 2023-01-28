0e32e4c28b88175d7680ed9324cb96a76ae3a475
Hosseinreza <hrakbari77@gmail.com>
2023-01-28 03:53:21 +0330

    Add boilerplate

--- app/app.js
+++ app/app.js
@@ -1,2 +1,14 @@
 // npm init
 // npm install express
+
+const express = require ("express");
+const https = require("https");
+const app = express();
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on http://localhost:3000")
+});
