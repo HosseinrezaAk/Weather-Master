4431decdd207e10ae26c62921486bdfa83dc3b11
Hosseinreza <hrakbari77@gmail.com>
2023-01-28 03:53:33 +0330

    Add get for root

--- app/app.js
+++ app/app.js
@@ -5,7 +5,14 @@ const express = require ("express");
 const https = require("https");
 const app = express();
 
+app.get("/", function( req, res){
 
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=40d5fe1720ee3ca82704129f2af71f0b#";
+    https.get( url, function(){
+        
+    });
+
+});
 
 
 
